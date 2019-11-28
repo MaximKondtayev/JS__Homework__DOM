46c4fff1ea001cb181c7c11f3e42556b84cb4a0f
Max <assassins08051998@gmIl.com>
2019-11-28 21:22:44 +0300

    commitsecond

--- JS__02/02.js
+++ JS__02/02.js
@@ -1,3 +1,18 @@
 // 2. Создать HTML-страницу с кнопкой "Открыть" и модальным окном. На модальном окне должен быть текст и кнопка "Закрыть". Изначально модальное окно не отображается. При клике на кнопку "Открыть" появляется модальное окно, на кнопку "Закрыть" – исчезает.
 
 
+let WindowOpen = document.getElementById("WindowOpen");
+let modal = document.getElementsByClassName("modal")[0];
+let WindowClose = document.getElementById("WindowClose");
+
+WindowOpen.addEventListener("click", Open);
+WindowClose.addEventListener("click", Close);
+
+
+function Open() {
+    modal.style.display = "block";
+}
+function Close() {
+    modal.style.display = "none";
+
+}
